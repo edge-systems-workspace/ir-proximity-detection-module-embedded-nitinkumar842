9da30ad7fc31abf3bd03e5b78243eaa42e159053
nitinkumar <nitinchaudhary0063@gmail.com>
2026-02-19 20:56:11 +0530

    updated details in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,53 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Nitin Kumar
+ * @date 2026-02-18
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
- */
+ * Provide lightweight stubs for editors/linters that can't resolve Arduino symbols.
+ * These are only used when the ARDUINO macro is not defined (i.e., not a real build).
+  */
+#ifndef ARDUINO
+// Pin modes
+#define INPUT 0
+#define OUTPUT 1
+// Digital levels
+#define LOW 0
+#define HIGH 1
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+inline void pinMode(int, int) {}
+inline int digitalRead(int) { return HIGH; }
+inline void digitalWrite(int, int) {}
 
- // TODO 2:
- // Create variable to store sensor state
+struct _SerialStub {
+    void begin(unsigned long) {}
+    void println(const char*) {}
+} Serial;
+
+inline void delay(unsigned long) {}
+#endif
+
+#define IR_PIN 2
+#define LED_PIN 13
 
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
+    pinMode(IR_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
+    int irState = digitalRead(IR_PIN);
 
-    // TODO 6:
-    // Read digital value from IR sensor
+    if (irState == LOW) {
+        digitalWrite(LED_PIN, HIGH);
+        Serial.println("Object Detected!");
+    } else {
+        digitalWrite(LED_PIN, LOW);
+    }
 
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
+    delay(100);
 }
